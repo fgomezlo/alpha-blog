ff3a81a4ad650087569469cb071cafc74b5961da
FRANCISCO GOMEZ LOPEZ <franciscogomezlopez@FRANCISCOs-MacBook-Pro-2.local>
2018-02-23 22:33:36 -0400

    style for messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  
+
   before_action :set_article, only: [:edit, :show, :update, :destroy]
 
   def index
@@ -11,25 +11,25 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    
+
   end
 
   def create
       #render plain: params[:article].inspect
       @article = Article.new(article_params)
       if @article.save
-        flash[:notice] = "Article was successfully created"
+        flash[:success] = "Article was successfully created"
         redirect_to article_path(@article)
       else
         render 'new'
       end
-      
+
   end
 
   def update
-    
+
     if @article.update article_params
-      flash[:notice] = "Article was successfully updated"
+      flash[:success] = "Article was successfully updated"
       redirect_to article_path(@article)
     else
       render 'edit'
@@ -38,12 +38,12 @@ class ArticlesController < ApplicationController
 
   def destroy
     @article.destroy
-    flash[:notice] = "Article was successfully deleted"
+    flash[:warning] = "Article was successfully deleted"
     redirect_to articles_path
   end
 
   def show
-    
+
   end
 
   private
