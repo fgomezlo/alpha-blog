82ac5a1f43c3d12fc671a80d605550030aaf24eb
FRANCISCO GOMEZ LOPEZ <franciscogomezlopez@FRANCISCOs-MacBook-Pro-2.local>
2018-02-26 12:38:01 -0400

    add show info articles with user who create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,18 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def index
+    @users = User.all
+  end
+
+  def edit
+
+  end
+
+  def show
+
+  end
+
   def create
     @user = User.new(user_params)
 
@@ -14,10 +26,6 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
-  end
-
-  def edit
-
   end
 
   def update
