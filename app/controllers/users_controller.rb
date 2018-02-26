f80b0a978ef6112853682fb67d9f820499179108
FRANCISCO GOMEZ LOPEZ <franciscogomezlopez@FRANCISCOs-MacBook-Pro-2.local>
2018-02-26 11:15:08 -0400

    add edit profile user

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,42 @@
+class UsersController < ApplicationController
+
+  before_action :set_article, only: [:edit, :show, :update, :destroy]
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:success] = "Welcome to the alpha blog #{@user.username}"
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+
+    if @user.update user_params
+      flash[:success] = "User was successfully updated"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
+  private
+    def set_article
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:username, :email, :password)
+    end
+
+end
